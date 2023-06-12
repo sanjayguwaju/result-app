a0ccb30125efd780ccceb321a61788257a89a7c1
sanjayguwaju <sanjayguwaju@gmail.com>
2023-06-12 19:21:31 +0545

    best results for app

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,19 @@ import React, { useState } from 'react';
 import html2pdf from 'html2pdf.js';
 import { saveAs } from 'file-saver';
 import './App.css';
+import { dummyResults } from './dummyResults';
 
 const App = () => {
   const [symbolNo, setSymbolNo] = useState('');
   const [name, setName] = useState('');
   const [dob, setDob] = useState('');
   const [batchYear, setBatchYear] = useState('');
-  const [results, setResults] = useState([
-    {
-      symbolNo: '001',
-      name: 'John Doe',
-      dob: '1995-07-15',
-      batchYear: '2022',
-      subjects: [
-        { name: 'Math', marks: 80, totalMarks: 100, passMarks: 40 },
-        { name: 'Science', marks: 75, totalMarks: 100, passMarks: 40 },
-        { name: 'English', marks: 90, totalMarks: 100, passMarks: 40 },
-      ],
-      passMarksCriteria: 40,
-      fullMarksCriteria: 100,
-    },
-    // Add more dummy results here
-  ]);
+  const [results, setResults] = useState(dummyResults);
   const [selectedResult, setSelectedResult] = useState(null);
 
   const calculatePercentage = (result) => {
-    const totalMarks = result.subjects.reduce((total, subject) => total + subject.totalMarks, 0);
-    const obtainedMarks = result.subjects.reduce((total, subject) => total + subject.marks, 0);
+    const totalMarks = result.subjects.reduce((total, subject) => total + subject.fullMarks, 0);
+    const obtainedMarks = result.subjects.reduce((total, subject) => total + subject.theoryMarks + subject.practicalMarks, 0);
     return ((obtainedMarks / totalMarks) * 100).toFixed(2);
   };
 
@@ -36,14 +22,17 @@ const App = () => {
     const result = results.find((result) => result.symbolNo === symbolNo);
     if (result) {
       const percentage = calculatePercentage(result);
-      const passMarksCriteria = result.passMarksCriteria;
-      const fullMarksCriteria = result.fullMarksCriteria;
-      const obtainedMarks = result.subjects.reduce((total, subject) => total + subject.marks, 0);
-      const passFailStatus = obtainedMarks >= passMarksCriteria ? 'Passed' : 'Failed';
+      const passStatus = percentage >= result.subjects[0].passMarks ? 'Passed' : 'Failed';
 
       const resultHTML = `
         <div class="result-container">
           <h2>Result</h2>
+          <p><strong>Class:</strong> ${result.grade}</p>
+          <p><strong>Roll No:</strong> ${result.section}</p>
+          <p><strong>Gender:</strong> ${result.gender}</p>
+          <p><strong>School Name:</strong> ABC School</p>
+          <p><strong>School Address:</strong> 123 Main Street, City</p>
+          <p><strong>Exam:</strong> 2nd Terminal Examination</p>
           <p><strong>Symbol No:</strong> ${result.symbolNo}</p>
           <p><strong>Name:</strong> ${result.name}</p>
           <p><strong>Date of Birth:</strong> ${result.dob}</p>
@@ -55,30 +44,31 @@ const App = () => {
                 <th>Subject</th>
                 <th>Full Marks</th>
                 <th>Pass Marks</th>
-                <th>Marks</th>
+                <th>Theory Marks</th>
+                <th>Practical Marks</th>
+                <th>Marks Obtained</th>
               </tr>
             </thead>
             <tbody>
               ${result.subjects
                 .map(
                   (subject) => `
-                <tr>
-                  <td>${subject.name}</td>
-                  <td>${subject.totalMarks}</td>
-                  <td>${subject.passMarks}</td>
-                  <td>${subject.marks}</td>
-                </tr>
-              `
+                  <tr>
+                    <td>${subject.name}</td>
+                    <td>${subject.fullMarks}</td>
+                    <td>${subject.passMarks}</td>
+                    <td>${subject.theoryMarks}</td>
+                    <td>${subject.practicalMarks}</td>
+                    <td>${Math.min(subject.theoryMarks + subject.practicalMarks, subject.fullMarks)}</td>
+                  </tr>
+                `
                 )
                 .join('')}
             </tbody>
           </table>
           <p><strong>Percentage:</strong> ${percentage}%</p>
-          <p><strong>Pass Marks Criteria:</strong> ${passMarksCriteria}</p>
-          <p><strong>Full Marks Criteria:</strong> ${fullMarksCriteria}</p>
-          <p><strong>Status:</strong> ${passFailStatus}</p>
-          <p><strong>Obtained Marks:</strong> ${obtainedMarks}</p>
-          <p><strong>School Name:</strong> ABC School</p>
+          <p><strong>Pass Marks Criteria:</strong> ${result.subjects[0].passMarks}</p>
+          <p><strong>Pass Status:</strong> ${passStatus}</p>
           <img src="school-logo.png" alt="School Logo" />
         </div>
       `;
@@ -95,69 +85,6 @@ const App = () => {
       html2pdf().from(resultHTML).set(opt).save();
     } else {
       setSelectedResult(null);
-    }
-  };
-
-  const downloadResult = () => {
-    const result = results.find((result) => result.symbolNo === symbolNo);
-    if (result) {
-      const percentage = calculatePercentage(result);
-      const passMarksCriteria = result.passMarksCriteria;
-      const fullMarksCriteria = result.fullMarksCriteria;
-      const obtainedMarks = result.subjects.reduce((total, subject) => total + subject.marks, 0);
-      const passFailStatus = obtainedMarks >= passMarksCriteria ? 'Passed' : 'Failed';
-
-      const resultHTML = `
-        <div class="result-container">
-          <h2>Result</h2>
-          <p><strong>School Name:</strong> ABC School</p>
-          <img src="school-logo.png" alt="School Logo" />
-          <p><strong>Symbol No:</strong> ${result.symbolNo}</p>
-          <p><strong>Name:</strong> ${result.name}</p>
-          <p><strong>Date of Birth:</strong> ${result.dob}</p>
-          <p><strong>Batch Year:</strong> ${result.batchYear}</p>
-          <h3>Subjects:</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>Subject</th>
-                <th>Full Marks</th>
-                <th>Pass Marks</th>
-                <th>Marks</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${result.subjects
-                .map(
-                  (subject) => `
-                <tr>
-                  <td>${subject.name}</td>
-                  <td>${subject.totalMarks}</td>
-                  <td>${subject.passMarks}</td>
-                  <td>${subject.marks}</td>
-                </tr>
-              `
-                )
-                .join('')}
-            </tbody>
-          </table>
-          <p><strong>Percentage:</strong> ${percentage}%</p>
-          <p><strong>Pass Marks Criteria:</strong> ${passMarksCriteria}</p>
-          <p><strong>Full Marks Criteria:</strong> ${fullMarksCriteria}</p>
-          <p><strong>Status:</strong> ${passFailStatus}</p>
-          <p><strong>Obtained Marks:</strong> ${obtainedMarks}</p>
-        </div>
-      `;
-
-      const opt = {
-        margin: 1,
-        filename: 'result.pdf',
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2 },
-        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-      };
-
-      html2pdf().from(resultHTML).set(opt).save();
     }
   };
 
@@ -184,11 +111,6 @@ const App = () => {
         <button type="button" onClick={generateResult}>
           Generate Result
         </button>
-        {selectedResult && (
-          <button type="button" onClick={downloadResult}>
-            Download Result
-          </button>
-        )}
       </form>
 
       {selectedResult && (
@@ -196,6 +118,9 @@ const App = () => {
           <h2>Result</h2>
           <p>
             <strong>School Name:</strong> ABC School
+          </p>
+          <p>
+            <strong>School Address:</strong> 123 Main Street, City
           </p>
           <img src="school-logo.png" alt="School Logo" />
           <p>
@@ -217,16 +142,20 @@ const App = () => {
                 <th>Subject</th>
                 <th>Full Marks</th>
                 <th>Pass Marks</th>
-                <th>Marks</th>
+                <th>Theory Marks</th>
+                <th>Practical Marks</th>
+                <th>Marks Obtained</th>
               </tr>
             </thead>
             <tbody>
               {selectedResult.subjects.map((subject) => (
                 <tr key={subject.name}>
                   <td>{subject.name}</td>
-                  <td>{subject.totalMarks}</td>
+                  <td>{subject.fullMarks}</td>
                   <td>{subject.passMarks}</td>
-                  <td>{subject.marks}</td>
+                  <td>{subject.theoryMarks}</td>
+                  <td>{subject.practicalMarks}</td>
+                  <td>{Math.min(subject.theoryMarks + subject.practicalMarks, subject.fullMarks)}</td>
                 </tr>
               ))}
             </tbody>
@@ -235,18 +164,10 @@ const App = () => {
             <strong>Percentage:</strong> {calculatePercentage(selectedResult)}%
           </p>
           <p>
-            <strong>Pass Marks Criteria:</strong> {selectedResult.passMarksCriteria}
+            <strong>Pass Marks Criteria:</strong> {selectedResult.subjects[0].passMarks}
           </p>
           <p>
-            <strong>Full Marks Criteria:</strong> {selectedResult.fullMarksCriteria}
-          </p>
-          <p>
-            <strong>Status:</strong>{' '}
-            {selectedResult.subjects.reduce((total, subject) => total + subject.marks, 0) >= selectedResult.passMarksCriteria ? 'Passed' : 'Failed'}
-          </p>
-          <p>
-            <strong>Obtained Marks:</strong>{' '}
-            {selectedResult.subjects.reduce((total, subject) => total + subject.marks, 0)}
+            <strong>Pass Status:</strong> {calculatePercentage(selectedResult) >= selectedResult.subjects[0].passMarks ? 'Passed' : 'Failed'}
           </p>
         </div>
       )}
---
+++ src/dummyResults.js
@@ -0,0 +1,67 @@
+export const dummyResults = [
+    {
+      symbolNo: '12345',
+      name: 'John Doe',
+      dob: '2005-01-01',
+      batchYear: '2023',
+      class: '10',
+      section: 'A',
+      gender: 'Male',
+      subjects: [
+        {
+          name: 'Mathematics',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 70,
+          practicalMarks: 0,
+        },
+        {
+          name: 'Science',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 65,
+          practicalMarks: 25,
+        },
+        {
+          name: 'English',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 80,
+          practicalMarks: 0,
+        },
+      ],
+    },
+    {
+      symbolNo: '54321',
+      name: 'Jane Smith',
+      dob: '2006-02-15',
+      batchYear: '2023',
+      class: '10',
+      section: 'B',
+      gender: 'Female',
+      subjects: [
+        {
+          name: 'Mathematics',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 50,
+          practicalMarks: 0,
+        },
+        {
+          name: 'Science',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 60,
+          practicalMarks: 30,
+        },
+        {
+          name: 'English',
+          fullMarks: 100,
+          passMarks: 40,
+          theoryMarks: 75,
+          practicalMarks: 0,
+        },
+      ],
+    },
+  ];
+  
